5088077b4f956984be2104580ca7bee0ac0b64df
Tóth Máté <toth.mate@icellkft.hu>
2022-04-11 09:03:55 +0200

    feat: convertToBinaryArray added

--- android/src/main/java/com/visioncameracodescanner/VisionCameraCodeScannerPlugin.java
+++ android/src/main/java/com/visioncameracodescanner/VisionCameraCodeScannerPlugin.java
@@ -214,22 +214,27 @@ public class VisionCameraCodeScannerPlugin extends FrameProcessorPlugin {
     map.putMap("content", convertContent(barcode));
     map.putInt("format", barcode.getFormat());
 
+    byte[] rawBytes = barcode.getRawBytes();
+    if (rawBytes != null) {
+      map.putArray("binary", convertToBinaryArray(rawBytes));
+    }
+
     return map;
   }
 
   // Bitmap Inversion https://gist.github.com/moneytoo/87e3772c821cb1e86415
   private Bitmap invert(Bitmap src)
-	{ 
+	{
 		int height = src.getHeight();
-		int width = src.getWidth();    
+		int width = src.getWidth();
 
 		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 		Canvas canvas = new Canvas(bitmap);
 		Paint paint = new Paint();
-		
+
 		ColorMatrix matrixGrayscale = new ColorMatrix();
 		matrixGrayscale.setSaturation(0);
-		
+
 		ColorMatrix matrixInvert = new ColorMatrix();
 		matrixInvert.set(new float[]
 		{
@@ -239,10 +244,10 @@ public class VisionCameraCodeScannerPlugin extends FrameProcessorPlugin {
 			0.0f, 0.0f, 0.0f, 1.0f, 0.0f
 		});
 		matrixInvert.preConcat(matrixGrayscale);
-		
+
 		ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrixInvert);
 		paint.setColorFilter(filter);
-		
+
 		canvas.drawBitmap(src, 0, 0, paint);
 		return bitmap;
 	}
